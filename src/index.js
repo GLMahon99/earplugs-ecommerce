54c1361400c70a7235030b480ac7231d9e9ade4e
GLMahon99 <gastimahon@gmail.com>
2026-03-19 14:53:45 -0300

    chore: remove firebase dependency and unused files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './firebase/firebase';
 import { BrowserRouter } from 'react-router-dom';
 import { ProductsProvider } from './context/Context';
 
